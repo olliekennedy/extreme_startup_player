915a036e9fe56203003ae3a0318d5629fc196f0a
okenned3@ford.com <okenned3@ford.com>
2024-07-18 16:07:15 +0100

    more MOOOOOOOOOOOOOOOO

--- src/main/java/com/oocode/HomePage.java
+++ src/main/java/com/oocode/HomePage.java
@@ -5,7 +5,7 @@ public class HomePage {
             <html>
                 <body>
                 <h1>Extreme startup player</h1>
-                <h2>MOOOOOOOOOO</h2>
+                <h2>MOOOOOOOOOOO</h2>
                 <p>This is a minimal web app starting point for <a href='https://extreme-startup.fly.dev/'>Extreme startup</a></p>
                 </body>
             </html>""".trim();
