2a86bc2b0bf01df8b252431d8d1e90070ccf5c35
Ollie Kennedy <okenned3@ford.com>
2024-07-26 10:31:53 +0100

    words not listOfWords

--- src/main/java/com/oocode/Answerer.java
+++ src/main/java/com/oocode/Answerer.java
@@ -4,38 +4,38 @@ import java.math.BigInteger;
 
 public class Answerer {
     public String answerFor(String question) {
-        String[] listOfWords = question.substring(0, question.length() - 1).split(" ");
+        String[] words = question.substring(0, question.length() - 1).split(" ");
 
         if (question.matches("^What is your name\\?$")) {
             return "Jack will win this for us";
         }
 
         if (question.matches("^What is \\d+ plus \\d+\\?$")) {
-            return sumOf(listOfWords[2], listOfWords[4]);
+            return sumOf(words[2], words[4]);
         }
 
         if (question.matches("^What is \\d+ multiplied by \\d+\\?$")) {
-            return Integer.toString(Integer.parseInt(listOfWords[2]) * Integer.parseInt(listOfWords[5]));
+            return Integer.toString(Integer.parseInt(words[2]) * Integer.parseInt(words[5]));
         }
 
         if (question.matches("^What is \\d+ minus \\d+\\?$")) {
-            return Integer.toString(Integer.parseInt(listOfWords[2]) - Integer.parseInt(listOfWords[4]));
+            return Integer.toString(Integer.parseInt(words[2]) - Integer.parseInt(words[4]));
         }
 
         if (question.matches("^What is \\d+ multiplied by \\d+ plus \\d+\\?$")) {
-            return Integer.toString(((Integer.parseInt(listOfWords[2]) * Integer.parseInt(listOfWords[5])) + Integer.parseInt(listOfWords[7])));
+            return Integer.toString(((Integer.parseInt(words[2]) * Integer.parseInt(words[5])) + Integer.parseInt(words[7])));
         }
 
         if (question.matches("^What is \\d+ plus \\d+ multiplied by \\d+\\?$")) {
-            return Integer.toString((Integer.parseInt(listOfWords[2]) + (Integer.parseInt(listOfWords[4]) * Integer.parseInt(listOfWords[7]))));
+            return Integer.toString((Integer.parseInt(words[2]) + (Integer.parseInt(words[4]) * Integer.parseInt(words[7]))));
         }
 
         if (question.matches("^What is \\d+ plus \\d+ plus \\d+\\?$")) {
-            return sumOf(listOfWords[2], listOfWords[4], listOfWords[6]);
+            return sumOf(words[2], words[4], words[6]);
         }
 
         if (question.matches("^What is \\d+ to the power of \\d+\\?$")) {
-            return new BigInteger(listOfWords[2]).pow(Integer.parseInt(listOfWords[7])).toString();
+            return new BigInteger(words[2]).pow(Integer.parseInt(words[7])).toString();
         }
 
         return "foo";
